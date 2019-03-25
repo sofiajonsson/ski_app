9a30234ca58bb2848257b80795e361c8f0569652
github username <github email address>
2019-03-25 15:28:08 -0700

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,11 +20,11 @@ listing = [
   {title: "Armada JJs", description: " Men's 186 Armada JJ's. Used for two seasons. Have a lot of life left in them.", image_url: " ", user_id: 1},
   {title: "Dynafit Boots", description: "Women's Dynafit touring boots. Used once, decided touring wasn't for me", image_url: " ", user_id: 5},
   {title:"POC helmet", description:"Decided I wanted a different color, never used", image_url: "", user_id: 3},
-  {title:"Smith Goggles", description:" Lightly used. Never left in car overnight, no fogging issues", image_url: " ", user_id: 2},
+  {title:"Smith Goggles", description: "Lightly used. Never left in car overnight, no fogging issues", image_url: " ", user_id: 2},
   {title:"Jump Suit", description:"Retro 80's jumpsuit. Size Medium. Great for end of season ski costumes", image_url: " ", user_id: 2},
   {title:"Beacon", description:"Mammut Barryvox S Beacon", image_url: " ", user_id: 1},
-  {title:"Shovel", description:"Black Diamond back country shovel. $20 obo", image_url: "", user_id: 3},
-  {title:"BC Pack", description:"Airbag never deployed, asking $200 O.B.O ", image_url: "", user_id: 1},
+  {title:"Shovel", description:"Black Diamond back country shovel. $20 obo", image_url: " ", user_id: 3},
+  {title:"BC Pack", description:"Airbag never deployed, asking $200 O.B.O ", image_url: " ", user_id: 1},
   {title:"XC skis", description:"Rossignol evo cross country skis, 2015 model, size M", image_url: " ", user_id: 2},
   {title:"Poles", description:"Salomon Arctic S3 Ski poles, size 110cm", image_url: " ", user_id: 4}
 ]
@@ -49,8 +49,8 @@ cart = [
 	{listing_id: 3, user_id: 2}
 
 user_rating = [
-  {listing_id: 3, user_id: 1},
-  {listing_id: 5, user_id: 2},
-  {listing_id: 2, user_id: 3},
-  {listing_id: 7, user_id: 4},
-	{listing_id: 9, user_id: 5}
+  {user_id: 3, user_id: 1, rating: 7},
+  {user_id: 5, user_id: 2, rating: 8},
+  {user_id: 2, user_id: 3, rating: 9},
+  {user_id: 7, user_id: 4, rating: 10},
+	{user_id: 9, user_id: 5, rating: 6}
