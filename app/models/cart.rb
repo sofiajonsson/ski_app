1e935bf6a556ed06b55ea9ea69ac6b05fa48a4a6
github username <github email address>
2019-03-25 14:55:37 -0700

    models corrected

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,4 @@
 class Cart < ApplicationRecord
+  has_many :listings
+  has_many :users
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,5 @@
 class Listing < ApplicationRecord
+  belongs_to :user
+  belongs_to :cart
+  has_many :comments
 end
