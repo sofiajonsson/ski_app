b82a9ba3e3a1cfc353472cb28f0eaef13e49823c
github username <github email address>
2019-03-25 17:18:44 -0700

    adding things and things

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,40 @@
 class ListingsController < ApplicationController
+before_action :set_listing, only: [:show, :edit, :update, :destroy]
+
+  def index
+
+  end
+
+  def show
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
+
+  def cart_params
+    params.require(:listing).permit(:title, :description, :user_id, :image_url, :)
+  end
 end
