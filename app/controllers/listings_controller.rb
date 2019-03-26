8729f8997b3f0331c3295be8c36c079e5c24619c
github username <github email address>
2019-03-26 08:47:59 -0700

    worked out listings view page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,26 +2,35 @@ class ListingsController < ApplicationController
 before_action :set_listing, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    @listings = Listing.all
   end
 
   def show
+    # @user = User.find(params[:user][:id])
   end
 
   def new
-
+    @listing = Listing.new()
   end
 
   def create
-
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      redirect_to @listing
+    else
+      render :new
+    end
   end
 
   def edit
-
   end
 
   def update
-
+    if @listing.update(listing_params)
+      redirect_to @listing
+    else
+      redirect_to :edit
+    end
   end
 
   def destroy
@@ -35,6 +44,6 @@ before_action :set_listing, only: [:show, :edit, :update, :destroy]
   end
 
   def cart_params
-    params.require(:listing).permit(:title, :description, :user_id, :image_url, :)
+    params.require(:listing).permit!
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+before_action :set_user, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@users = User.all
 	end
@@ -15,17 +17,14 @@ class UsersController < ApplicationController
 			render :new
 		end
 	end
-	 
+
 	def show
-		@user =User.find(params[:id])
 	end
 
 	def edit
-		@user =User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update(user_params)
 				redirect_to @user
 			else
@@ -34,6 +33,10 @@ class UsersController < ApplicationController
 	end
 
 	private
+
+	def set_user
+		@user = User.find(params[:id])
+	end
 
 	def user_params
 		params.require(:user).permit!
