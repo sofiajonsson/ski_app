22f76e289c5867024efc3dfedcbb8404ec0c6487
Sofia <jonssonsofia93@gmail.com>
2019-03-29 09:18:23 -0700

    routes, home image, no more flash errors on pages

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   resources :users
   get "/login", to: "sessions#new"
   post '/login', to: "sessions#create"
+  get "/home", to: "home#index"
   post "/add_to_cart", to: "carts#new"
    delete '/logout', to: 'sessions#destroy'
   get "/users_cart", to: "carts#users_cart"
